1a031452f4403b58e20e561fecbc0b391361eebe
Jason Warner <jcwarner@gmail.com>
2018-04-25 21:44:17 -0500

    updated file routes

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ let waitList = [];
 
 // Routes 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/tables", function (req, res) {
-    res.sendFile(path.join(__dirname, "../tables.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "../reserve.html"));
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // Displays table reservations
